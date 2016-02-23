f2f972f5d88e36039cb9ac2adcf1d51871b05ec1
Prem Patel <prem.patel3105@gmail.com>
2016-02-23 12:10:31 +0530

    Add angular services

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var app = angular.module('myStory', []);
 
-app.controller('MainCtrl', ['$scope',function($scope){
+app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
   $scope.test = 'Hello world!';
+  $scope.posts = posts.posts;
 
-  $scope.posts = [
+  /*$scope.posts = [
     {title: 'post 1', upvotes: 5},
     {title: 'post 2', upvotes: 2},
     {title: 'post 3', upvotes: 15},
     {title: 'post 4', upvotes: 9},
     {title: 'post 5', upvotes: 4}
-  ];
+  ];*/
 
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === '') { return; }
@@ -20,10 +21,18 @@ app.controller('MainCtrl', ['$scope',function($scope){
     });
     $scope.title = '';
     $scope.link = '';
+    console.log($scope.posts);
   };
 
   $scope.incrementUpvotes = function(post){
     post.upvotes += 1;
   };
 
+}]);
+
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
 }]);
